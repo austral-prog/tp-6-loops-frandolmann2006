a0c278cfee1a49335edd0694c278ec5730cf6ba5
frandolmann2006 <frankdolmann@gmail.com>
2026-04-27 19:54:45 -0300

    Update list_stats.py

--- list_stats.py
+++ list_stats.py
@@ -26,7 +26,8 @@ def range_of(numbers):
 
     Ejemplo: range_of([3, 1, 7, 2]) -> 6  (7 - 1)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+
+    return find_max(numbers)-find_min(numbers)  # Remove this line and implement
 
 
 def average(numbers):
@@ -37,7 +38,12 @@ def average(numbers):
 
     Ejemplo: average([10, 20, 30]) -> 20.0
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(numbers)==0:
+        return 0.0
+    total = 0
+    for numero in numbers:
+        total += numero
+    return round(total / len(numbers), 1)  # Remove this line and implement
 
 
 def describe(numbers):
@@ -50,4 +56,7 @@ def describe(numbers):
 
     Ejemplo: describe([3, 1, 7, 2]) -> "Min:1 Max:7 Range:6 Avg:3.2"
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(numbers) == 0:
+        return "Empty list"
+    return f"Min:{find_min(numbers)} Max:{find_max(numbers)} Range:{range_of(numbers)} Avg:{average(numbers)}"  # Remove this line and implement
+
